4a486a23c9427d6194e96a8dbc4d2ebc161573b5
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2026-09-21 00:26:23 +0700

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,17 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: { bodySizeLimit: "2mb" },
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
