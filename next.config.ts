9e816800f64c3acb7e0c9077b7f2eaaa15884be8
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2026-09-21 00:03:36 +0700

    Fix build config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
   experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
+    serverActions: { bodySizeLimit: "2mb" },
   },
 };
 
